83929d7a54323e1ce20ae69612c526b918cf3cd2
Nicolas Colombo <nicolas100533@gmail.com>
2023-06-23 15:52:24 -0300

    .

--- index.js
+++ index.js
@@ -18,9 +18,7 @@ app.set("json spaces", 2);
 app.use(morgan("dev"));
 app.use(express.urlencoded({extended:false}));
 app.use(express.json());
-app.use(cors({
-    origin: ["/\.fetchprojectncolombo\.vercel\.app$/", "http://localhost:3000"],
-}));
+app.use(cors());
 
 app.use(cookieParser());
 
